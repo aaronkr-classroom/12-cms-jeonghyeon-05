dd62cdd10592cdc810246586b9f6fa9396f82ce1
에런스노버거 <jekkilekki@gmail.com>
2026-05-29 17:15:33 +0900

    Add files via upload

---
+++ c12/cms/includes/functions_TODO.php
@@ -0,0 +1,15 @@
+<?php
+// @TODO: DATABASE FUNCTION
+
+
+// @TODO: FORMATTING FUNCTIONS
+
+
+// @TODO: ERROR AND EXCEPTION HANDLING FUNCTIONS
+// Convert errors to exceptions
+
+
+// Handle exceptions - log exception and show error message (if server does not send error page listed in .htaccess)
+
+
+// Handle fatal errors
